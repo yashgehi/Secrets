9cdb805408f186621b4931ff1ba7308d085b5104
Yash Gehi <yashgehi398@gmail.com>
2020-06-25 04:24:15 +0530

    level 3 security

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs')
 const mongoose = require('mongoose')
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -17,7 +17,37 @@ const userSchema = new mongoose.Schema({
     email:string,
     password:string
 });
-
+const User = mongoose.model("User",userSchema);
 const secret = process.env.SECRET;
 
 userSchema.plugin(encrypt,{secret:secret}, {encryptedFields : ['password']});
+
+app.post("/register",function(req,res){
+    const newUser = new userSchema({
+        email:(req.body.username),
+        password:md5(req.body.password)
+    });
+    newUser.save(function(err){
+        if(err){
+            console.log(err)
+        }else{
+            res.render("secret")
+        }
+    })
+});
+
+app.post("/login",function(req,res){
+    const newUser = new userSchema({
+        email: (req.body.username),
+        password: md5(req.body.password)
+    });
+    User.findOne({email:username},function(err,foundUser){
+        if(err){
+            console.log(err)
+        }else{
+            if(foundUser.password===password){
+                res.render("secret");
+            }
+        }
+    });
+});
