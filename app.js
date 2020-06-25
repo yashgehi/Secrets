cafae5aa558c11574f87223fbfe3db0ba2058460
Yash Gehi <yashgehi398@gmail.com>
2020-06-26 00:01:46 +0530

    second commit

--- app.js
+++ app.js
@@ -10,20 +10,33 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set('view engine','ejs');
 
-mongoose.connect("mongodb://localhost:27017/userDB")
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true })
 
 
 const userSchema = new mongoose.Schema({
-    email:string,
-    password:string
+    email:String,
+    password:String
 });
 const User = mongoose.model("User",userSchema);
+
+
+
+app.get("/",function(req,res){
+    res.render("home")
+});
+
+app.get("/login",function(req,res){
+    res.render("login")
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
 const secret = process.env.SECRET;
 
-userSchema.plugin(encrypt,{secret:secret}, {encryptedFields : ['password']});
-
 app.post("/register",function(req,res){
-    const newUser = new userSchema({
+    const newUser = new User({
         email:(req.body.username),
         password:md5(req.body.password)
     });
@@ -31,23 +44,27 @@ app.post("/register",function(req,res){
         if(err){
             console.log(err)
         }else{
-            res.render("secret")
+            res.render("secrets")
         }
     })
 });
 
 app.post("/login",function(req,res){
-    const newUser = new userSchema({
-        email: (req.body.username),
-        password: md5(req.body.password)
-    });
+    
+        const username = (req.body.username);
+        const password = md5(req.body.password);
+
     User.findOne({email:username},function(err,foundUser){
         if(err){
             console.log(err)
         }else{
             if(foundUser.password===password){
-                res.render("secret");
+                res.render("secrets");
             }
         }
     });
+});
+
+app.listen(3000,function(){
+    console.log("server started at 3000")
 });
